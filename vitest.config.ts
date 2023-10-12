a789cdf2ca11b0253056de4dc86ede6602871a76
Kevin Brown <kevin@webstackbuilders.com>
2023-10-12 14:50:24 +0300

    Update config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,23 @@
 /// <reference types="vitest" />
+// https://vitest.dev/config/
 import { getViteConfig } from 'astro/config'
+
+// @TODO: Should set up `reporters` for CI to create an artifact on failed runs with `outputFIle`
+
+// https://vitest.dev/guide/environment.html
 
 export default getViteConfig({
   test: {
-    // avoid globals imports (assert, describe, expect, test)
-    globals: true,
     include: ['src/**/*.spec.ts?(x)'],
+    environmentMatchGlobs: [
+      ['src/**', 'jsdom'],
+      ['src/lib/**', 'node'],
+    ],
+    // globalSetup: '',
+    // setupFiles: '',
+    coverage: {
+      enabled: false,
+      extension: ['.ts', '.tsx', '.astro'],
+    },
   },
 })
