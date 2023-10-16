3a16252ff5c3ea5a58e4d9e13277711e79ad5a6c
Kevin Brown <kevin@webstackbuilders.com>
2023-10-16 13:33:34 +0300

    Add Purge CSS integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
+import purgecss from "astro-purgecss"
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind({
       // don't autogenerate base style sheet and inject it
-      applyBaseStyles: false,
+      applyBaseStyles: false
     }),
     mdx(),
-  ],
+    // PurgeCSS should be last item in integrations array
+    purgecss(),
+  ]
 })
