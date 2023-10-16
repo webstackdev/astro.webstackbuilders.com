b3f44be94e17d580fe1caeb08bc6474d25ceb04f
Kevin Brown <kevin@webstackbuilders.com>
2023-10-16 13:35:29 +0300

    Add page prefetch integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,21 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import mdx from '@astrojs/mdx'
-import purgecss from "astro-purgecss"
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import purgecss from "astro-purgecss";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind({
       // don't autogenerate base style sheet and inject it
-      applyBaseStyles: false
+      applyBaseStyles: false,
     }),
     mdx(),
-    // PurgeCSS should be last item in integrations array
+    /** Prefetch page links when they are visible on screen */
+    prefetch(),
+    /** PurgeCSS should be last item in integrations array */
     purgecss(),
-  ]
+  ],
 })
