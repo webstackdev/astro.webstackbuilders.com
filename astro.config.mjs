b2b232011c3301d1a7cfbbff0d73f75f88a4c8fc
Kevin Brown <kevin@webstackbuilders.com>
2023-10-16 21:39:40 +0300

    Add icon sprites

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import purgecss from "astro-purgecss";
-
 import prefetch from "@astrojs/prefetch";
+
+import svgSprite from "astro-svg-sprite";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,9 +14,8 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
     mdx(),
-    /** Prefetch page links when they are visible on screen */
-    prefetch(),
-    /** PurgeCSS should be last item in integrations array */
-    purgecss(),
+    prefetch(), // Prefetch page links when the link becomes visible on screen
+    svgSprite(),
+    purgecss(), // PurgeCSS should be last item in integrations array
   ],
 })
