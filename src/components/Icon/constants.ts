7a1ae5eaf261771a5089bf4915768ee4fb6dd804
Kevin Brown <kevin@webstackbuilders.com>
2025-11-25 23:45:46 +0300

    Add e2e test and sample page for Icon component

---
+++ src/components/Icon/constants.ts
@@ -0,0 +1,14 @@
+export const colorClasses = {
+  default: 'text-text fill-text',
+  muted: 'text-text-offset fill-text-offset',
+  primary: 'text-primary fill-primary',
+  accent: 'text-accent fill-accent',
+  info: 'text-info fill-info',
+  success: 'text-success fill-success',
+  warning: 'text-warning fill-warning',
+  danger: 'text-danger fill-danger',
+  inherit: 'text-inherit fill-inherit',
+  custom: '',
+} as const
+
+export type IconVariant = keyof typeof colorClasses
