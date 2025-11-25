d1b5a8a3f30cf1badec2c23c28bd1f69ad72fd52
Kevin Brown <kevin@webstackbuilders.com>
2025-11-25 16:47:02 +0300

    Clean up lint errors in .vscode file for test automation

--- .vscode/dev-server-cli.mjs
+++ .vscode/dev-server-cli.mjs
@@ -1,101 +1,112 @@
 #!/usr/bin/env node
-import { accessSync, readFileSync, unlinkSync, writeFileSync } from "fs";
-import spawn from "cross-spawn";
-import tempDir from "temp-dir"; // /tmp
-import yargs from "yargs/yargs";
-import { hideBin } from "yargs/helpers";
+/* eslint-env node */
+/* global console, process */
+import { accessSync, readFileSync, unlinkSync, writeFileSync } from 'fs'
+import spawn from 'cross-spawn'
+import tempDir from 'temp-dir' // /tmp
+import yargs from 'yargs/yargs'
+import { hideBin } from 'yargs/helpers'
 
-const pidFile = `${tempDir}/debug-server-hmDAr917.pid`;
-const port = 3000;
+const pidFile = `${tempDir}/debug-server-hmDAr917.pid`
+const port = 3000
 
+/**
+ * Ensure we do not start a duplicate dev server instance
+ */
 const failOnPidFileExists = () => {
   try {
-    accessSync(pidFile);
-  } catch (err) {
-    return false;
+    accessSync(pidFile)
+  } catch {
+    return false
   }
   console.error(
     `[error] Server appears to be running, found PID file ${pidFile}`,
-  );
-  return process.exit(116);
-};
+  )
+  return process.exit(116)
+}
 
-const writePidFile = (pid) => {
+/**
+ * Persist the child process PID to allow stop command to find it later
+ */
+const writePidFile = pid => {
   try {
-    writeFileSync(pidFile, pid.toString(10), { flag: "ax" });
+    writeFileSync(pidFile, pid.toString(10), { flag: 'ax' })
   } catch (err) {
-    console.error(`[error] Failed to write PID ${pid} PID file ${pidFile}: ${err.message}`);
-    return process.exit(11);
+    console.error(`[error] Failed to write PID ${pid} PID file ${pidFile}: ${err.message}`)
+    return process.exit(11)
   }
 }
 
+/**
+ * Remove the PID file when the dev server stops
+ */
 const unlinkPidFile = () => {
   try {
-    unlinkSync(pidFile);
+    unlinkSync(pidFile)
   } catch (err) {
     console.error(
       `[error] Failed to delete PID file ${pidFile}: ${err.message}`,
-    );
-    return process.exit(11);
+    )
+    return process.exit(11)
   }
-};
+}
 
 yargs(hideBin(process.argv))
   .command(
-    "start",
-    "Start the server",
+    'start',
+    'Start the server',
     {},
     () => {
-      failOnPidFileExists();
+      failOnPidFileExists()
 
-      const child = spawn(`${process.cwd()}/node_modules/.bin/vercel`, ["dev", "--listen", port], {
+      const child = spawn(`${process.cwd()}/node_modules/.bin/vercel`, ['dev', '--listen', port], {
         detached: true,
-        stdio: "pipe",
-      });
+        stdio: 'pipe',
+      })
 
-      let isPidWritten = false;
+      let isPidWritten = false
 
-      child.on("error", (err) => {
-        console.log(err);
-      });
+      child.on('error', error => {
+        console.error(error)
+      })
 
       /** Forward child stdout to VS Code Task */
-      child.stdout.setEncoding("utf8");
-      child.stdout.on("data", (data) => {
+      child.stdout.setEncoding('utf8')
+      child.stdout.on('data', data => {
         if (!isPidWritten) {
-          writePidFile(child.pid);
-          isPidWritten = true;
+          writePidFile(child.pid)
+          isPidWritten = true
         }
-        console.log(data);
-      });
+        console.log(data)
+      })
 
-      child.stderr.setEncoding("utf8");
-      child.stderr.on("data", (error) => {
-        console.error(error);
-      });
+      child.stderr.setEncoding('utf8')
+      child.stderr.on('data', error => {
+        console.error(error)
+      })
 
-      child.on("close", (code) => {
-        child.unref();
-        return process.exit(code);
-      });
+      child.on('close', code => {
+        child.unref()
+        return process.exit(code)
+      })
     },
   )
   .command(
-    "stop",
-    "Stop the server",
+    'stop',
+    'Stop the server',
     {},
     () => {
       try {
-        const pid = readFileSync(pidFile, { encoding: "utf8", flag: "r" });
-        process.kill(parseInt(pid, 10), "SIGINT");
-        unlinkPidFile();
-        return process.exit(0);
+        const pid = readFileSync(pidFile, { encoding: 'utf8', flag: 'r' })
+        process.kill(parseInt(pid, 10), 'SIGINT')
+        unlinkPidFile()
+        return process.exit(0)
       } catch (err) {
         console.error(
           `[error] Failed to shut down server, reading PID file ${pidFile}: ${err.message}`,
-        );
-        return process.exit(11);
+        )
+        return process.exit(11)
       }
     },
   )
-  .parse();
+  .parse()
