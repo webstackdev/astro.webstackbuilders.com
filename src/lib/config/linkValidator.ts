32afc243dc83114151361a369099c1654a7bf7e0
Kevin Brown <kevin@webstackbuilders.com>
2026-01-04 17:59:29 +0300

    Make link check mandatory and increase time-out, also fix broken link to corp website with trailing slash

--- src/lib/config/linkValidator.ts
+++ src/lib/config/linkValidator.ts
@@ -4,7 +4,7 @@ export const linkValidatorPlugin = linkValidator({
   checkExternal: true,
   exclude: [
     /**
-     * Company social network links are manually maintained, rated
+     * Company social network links are manually maintained, rate
      * limited in CI so hard to check automatically
      */
     'https://bsky.app/profile/webstackdev.bsky.social',
@@ -17,13 +17,19 @@ export const linkValidatorPlugin = linkValidator({
     'https://www.linkedin.com/company/webstack-builders',
     'https://www.youtube.com/@webstackbuilders',
     'https://x.com/WebstackDev',
-    /** Requires HEAD request, not GET */
+    /** Requires HEAD request, not GET. Used in Add-to-Calendar component. */
     'https://outlook.office.com/calendar/0/deeplink/compose',
     /** Rate-limited URLs that will always fail */
     'https://www.npmjs.com',
     /** Dummy link in demo page */
     '/path/to#my-blob',
+    /** Skip any links in testing-only pages */
+    'https://www.webstackbuilders.com/testing/*',
+    /** Skip hard-coded site URLs due to timeouts from CI to Vercel */
+    'https://www.webstackbuilders.com',
+    'https://www.webstackbuilders.com/rss.xml',
   ],
+  externalTimeout: 15000,
   failOnBrokenLinks: true,
   verbose: true,
 })
