5017664dcc914843996764d10c35b78eee8102c8
Kevin Brown <kevin@webstackbuilders.com>
2026-02-11 18:11:53 +0300

    Finish refactor of components and test helpers to LitElements

--- src/components/scripts/@types/webComponentModule.ts
+++ src/components/scripts/@types/webComponentModule.ts
@@ -1,4 +1,6 @@
-export interface WebComponentModule<TElement extends HTMLElement = HTMLElement> {
+import type { LitElement } from 'lit'
+
+export interface WebComponentModule<TElement extends HTMLElement = LitElement> {
   registeredName: string
   componentCtor: CustomElementConstructor & { prototype: TElement }
   registerWebComponent: (_tagName?: string) => Promise<void> | void
