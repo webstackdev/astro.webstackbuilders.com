20c5c76e9c66769b5ea34997e6da7b05bc567bae
Kevin Brown <kevin@webstackbuilders.com>
2026-03-27 04:13:04 +0300

    Add h1 to newsletter confirmation state pages

--- test/e2e/specs/11-accessibility/axe.spec.ts
+++ test/e2e/specs/11-accessibility/axe.spec.ts
@@ -9,18 +9,20 @@ import { runAcrossPages } from '@test/e2e/helpers/runAcrossPages'
 // https://github.com/dequelabs/axe-core/blob/develop/doc/rule-descriptions.md?plain=1
 
 /*
-cat.aria: Rules related to Accessible Rich Internet Applications (ARIA) attributes and roles.
-cat.color: Rules related to color contrast and meaning conveyed by color.
-cat.controls: Rules for interactive controls, such as form elements and links.
-cat.forms: Rules specifically for forms, form fields, and their labels.
-cat.keyboard: Rules related to keyboard operability.
-cat.links: Rules for links, including their names and destinations.
-cat.name-role-value: Rules that check if an element has a name, role, and value that can be correctly interpreted by assistive technologies.
-cat.semantics: Rules related to the semantic structure of a document, such as headings and landmarks.
-cat.sensory-and-visual-cues: Rules that deal with information conveyed by sensory or visual characteristics.
-cat.structure: Rules related to the document's overall structure, like the proper nesting of elements.
-cat.tables: Rules for data tables, including headers and associations.
-cat.text-alternatives: Rules for ensuring that text alternatives are provided for non-text content, such as images.
+const axeTags = [
+  'cat.aria'  // Rules related to Accessible Rich Internet Applications (ARIA) attributes and roles.
+  'cat.color'  // Rules related to color contrast and meaning conveyed by color.
+  'cat.controls'  // Rules for interactive controls, such as form elements and links.
+  'cat.forms'  // Rules specifically for forms, form fields, and their labels.
+  'cat.keyboard'  // Rules related to keyboard operability.
+  'cat.links'  // Rules for links, including their names and destinations.
+  'cat.name-role-value'  // Rules that check if an element has a name, role, and value that can be correctly interpreted by assistive technologies.
+  'cat.semantics'  // Rules related to the semantic structure of a document, such as headings and landmarks.
+  'cat.sensory-and-visual-cues'  // Rules that deal with information conveyed by sensory or visual characteristics.
+  'cat.structure'  // Rules related to the document's overall structure, like the proper nesting of elements.
+  'cat.tables'  // Rules for data tables, including headers and associations.
+  'cat.text-alternatives'  // Rules for ensuring that text alternatives are provided for non-text content, such as images.
+]
 */
 
 describe('WCAG Compliance', () => {
