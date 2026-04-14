215bcaa53993bfbb203866b3a3423c6328c87c5f
Kevin Brown <kevin@webstackbuilders.com>
2026-04-14 06:01:37 +0300

    Fix test helper

--- src/lib/content/contactData.ts
+++ src/lib/content/contactData.ts
@@ -1,18 +1,8 @@
-import { getCollection, type CollectionEntry } from 'astro:content'
-import { BuildError } from '@lib/errors/BuildError'
-
-const CONTACT_DATA_FILE = 'src/lib/content/contactData.ts'
+import type { CollectionEntry } from 'astro:content'
+import rawContactData from '@content/contact.json'
 
 export type ContactData = CollectionEntry<'contactData'>['data']
 export type CompanyContactData = ContactData['company']
 
-const [contactDataEntry] = await getCollection('contactData')
-
-if (!contactDataEntry) {
-  throw new BuildError('Missing contact data entry', {
-    filePath: CONTACT_DATA_FILE,
-  })
-}
-
-export const contactData: ContactData = contactDataEntry.data
+export const contactData: ContactData = rawContactData
 export const companyContactData: CompanyContactData = contactData.company
