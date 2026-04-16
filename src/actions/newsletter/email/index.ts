05780a2c425a4be0b7404fdfde5d0cc5f770615c
Kevin Brown <kevin@webstackbuilders.com>
2026-04-17 00:53:33 +0300

    Remove unneeded *.ts email files from actions

--- src/actions/newsletter/email/index.ts
+++
@@ -1,4 +0,0 @@
-export { generateConfirmationEmailHtml } from './confirmationHtml'
-export { generateConfirmationEmailText } from './confirmationHtml'
-export { generateWelcomeEmailHtml } from './welcomeHtml'
-export { generateWelcomeEmailText } from './welcomeHtml'
